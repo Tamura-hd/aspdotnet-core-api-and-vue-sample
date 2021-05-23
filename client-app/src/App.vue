5f2d7816d46a7b1d0a20020991097eb5abb12401
Tamura-hd <42891598+Tamura-hd@users.noreply.github.com>
2021-05-23 06:51:51 +0000

    gl issue #1
    Implemented to list display.

---
+++ client-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <Header :leftDrawerOpen.sync="leftDrawerOpen" />
+    <Drawer :leftDrawerOpen="leftDrawerOpen" />
+
+    <q-page-container>
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Drawer from '@/components/Drawer.vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    Header,
+    Drawer,
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ client-app/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-header elevated class="glossy">
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        @click="$emit('update:leftDrawerOpen', !leftDrawerOpen)"
+        aria-label="Menu"
+        icon="menu"
+      />
+
+      <q-toolbar-title>
+        Quasar App
+      </q-toolbar-title>
+
+      <div>Quasar v{{ $q.version }}</div>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    leftDrawerOpen: Boolean,
+  },
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
